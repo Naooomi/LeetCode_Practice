308759eb5fb1e85db2df1e38a35ce53d349d5fb5
Naooomi <naomi.wuuu@gmail.com>
2017-11-29 22:34:21 +1300

    isPowerOfTwo Recursive

--- Math/isPowerOfTwo.java
+++ Math/isPowerOfTwo.java
@@ -15,4 +15,10 @@ class Solution {
       return (n == 1);
     }
 
+    /**
+      Solution 2: Recursive
+    */
+    public boolean isPowerOfTwo(int n) {
+      return n>0 && (n==1 || (n%2==0 && isPowerOfTwo(n/2)));
+    }
 }
