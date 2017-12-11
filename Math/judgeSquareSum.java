65b7a0b0a6760773644c6ea5e44a6e40d7e448fc
Naooomi <naomi.wuuu@gmail.com>
2017-12-11 23:47:26 +1300

    Sum of Square Numbers

---
+++ Math/judgeSquareSum.java
@@ -0,0 +1,24 @@
+/**
+
+  Description:
+
+  Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+
+*/
+class Solution {
+    public boolean judgeSquareSum(int c) {
+
+        int i = 0;
+        int j = (int) Math.sqrt(c);
+        while (i <= j) {
+            if ((i*i + j*j < c)) {
+                i++;
+            } else if (i*i + j*j > c) {
+                j--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
